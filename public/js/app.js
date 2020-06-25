1ccc1a7d7d134fccb95ea068c765fac9fc1bf701
suntzu1 <41553184+suntzu1@users.noreply.github.com>
2020-06-25 01:22:06 -0400

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('client side javascript is loaded');
+// console.log('client side javascript is loaded');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('#txt-address');
